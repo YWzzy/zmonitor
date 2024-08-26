368852d807ab53fed3165f9814ecdc7233e108cb
yinhan <1738348915@qq.com>
2024-08-26 11:17:29 +0800

    chome：去除无关模块

--- server/src/bookmarks/dto/update-bookmark.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBookmarkDto } from './create-bookmark.dto';
-
-export class UpdateBookmarkDto extends PartialType(CreateBookmarkDto) {}
